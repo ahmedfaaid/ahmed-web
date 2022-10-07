642228a6bc80076147ec21ac742803ef764f364d
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2022-10-06 23:42:11 -0400

    project data

---
+++ constants/projectData.ts
@@ -0,0 +1,56 @@
+export const projects = [
+  {
+    id: '1',
+    name: 'YelpCamp',
+    image: '/images/project/YelpCamp.png',
+    description:
+      'YelpCamp is a fictional campground info and review web app built with NodeJS, Express and MongoDB',
+    deployed: 'https://nameless-sands-78625.herokuapp.com/',
+    github: 'https://github.com/ahmedfaaid/yelp-camp-project'
+  },
+  {
+    id: '2',
+    name: "Beats'N'Vibes",
+    image: "/images/project/Beats'N'Vibes.png",
+    description:
+      "Beats'N'Vibes is a fictional DJ blog built using GatsbyJS, GraphQL and data sourced from Contentful CMS",
+    deployed: 'https://objective-wozniak-6dbb27.netlify.app/',
+    github: 'https://github.com/ahmedfaaid/beatsnvibes'
+  },
+  {
+    id: '3',
+    name: 'Sneakersseur',
+    image: '/images/project/sneakersseur.png',
+    description:
+      'Sneakersseur is a sneaker news website built with GatsbyJS, ReactJS, GraphQL and Markdown files',
+    deployed: 'https://sneakersseur.netlify.app/',
+    github: 'https://github.com/ahmedfaaid/sneak'
+  },
+  {
+    id: '4',
+    name: 'COVID-19 Tracker',
+    image: '/images/project/covid.png',
+    description:
+      'COVID-19 Tracker displays the latest global and country cases. It is built with NextJS with data from a REST API',
+    deployed: 'https://covid-19-ahmed.now.sh/',
+    github: 'https://github.com/ahmedfaaid/covid-19-tracker'
+  },
+  {
+    id: '5',
+    name: 'Amour Essentials',
+    image: '/images/project/amouressentials.png',
+    description:
+      'Amour Essentials is an e-commerce landing page built with NextJS and powered by Strapi and Cloudinary',
+    deployed: 'https://www.amouresntls.com/',
+    github: 'https://github.com/amour-mensah/amour-essentials'
+  },
+  {
+    id: '6',
+    name: 'Tritan Group',
+    image: '/images/project/tritan-group.png',
+    description:
+      "Tritan Group's company website and job board is built with NextJS content powered by Strapi",
+    deployed: 'https://tritan.ca',
+    github: 'https://github.com/ahmedfaaid/tritan-group'
+  }
+];
