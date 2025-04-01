7cd335e30fe08d1c917c70511c74e3c86b50d317
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-04-01 19:58:29 +0000

    remove unneeded mdx config

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-import createMdx from '@next/mdx';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import rehypeCodeTitles from 'rehype-code-titles';
-import rehypePrism from 'rehype-prism';
-import rehypeSlug from 'rehype-slug';
-import remarkGfm from 'remark-gfm';
+const nextConfig = {
+  reactStrictMode: true
+};
 
-const withMDX = createMdx({
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [
-      rehypeSlug,
-      rehypeCodeTitles,
-      rehypePrism,
-      [
-        rehypeAutolinkHeadings,
-        {
-          properties: {
-            className: ['anchor']
-          }
-        }
-      ]
-    ]
-  }
-});
-
-const nextConfig = withMDX({
-  reactStrictMode: true,
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx']
-});
 export default nextConfig;
