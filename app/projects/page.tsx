43a66f341ea608364d79975870583c3697e5bfe0
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-04-02 23:14:02 +0000

    projects page to app router

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,10 @@
-import Layout from 'components/layout';
-import Project from 'components/project';
-import { projects } from 'constants/projectData';
-import { Project as ProjectType } from 'types';
+import Project from '@/components/project';
+import { projects } from '@/constants/projectData';
+import { Project as ProjectType } from '@/types';
 
 export default function Projects() {
   return (
-    <Layout title='Projects'>
+    <>
       <section>
         <div className='mt-8 grid gap-4 px-4 sm:grid-cols-2 sm:px-0 md:mt-16 md:grid-cols-3 lg:mt-24'>
           {projects.map((project: ProjectType) => (
@@ -13,6 +12,6 @@ export default function Projects() {
           ))}
         </div>
       </section>
-    </Layout>
+    </>
   );
 }
