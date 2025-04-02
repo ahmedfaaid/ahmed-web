a14f68338ffe0b1df67c9d664c329b4464fd9770
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-04-02 19:33:31 +0000

    updated search params from search input value

--- components/search.tsx
+++ components/search.tsx
@@ -1,36 +1,69 @@
-import { Dispatch, SetStateAction } from 'react';
+'use client';
 
-interface SearchProps {
-  setSearchQuery: Dispatch<SetStateAction<string>>;
-  query: string;
-}
+import { useRouter, useSearchParams } from 'next/navigation';
+import {
+  ChangeEvent,
+  FormEvent,
+  useCallback,
+  useEffect,
+  useState
+} from 'react';
 
-export default function Search({ setSearchQuery, query }: SearchProps) {
+export default function Search() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const search = searchParams?.get('search');
+  const [inputValue, setInputValue] = useState('');
+
+  const searchTerm = useCallback(
+    (searchText: string) => {
+      router.replace(`/blog?search=${searchText}`);
+    },
+    [router]
+  );
+
+  useEffect(() => {
+    if (search && search.trim() !== '') {
+      setInputValue(search);
+    }
+  }, []);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    searchTerm(inputValue);
+  };
+
   return (
     <div className='flex w-full justify-center px-4 sm:px-0'>
-      <div className='relative w-[32rem]'>
-        <input
-          type='search'
-          className='w-full rounded-full py-4 pr-4 pl-14'
-          placeholder='Search posts'
-          onChange={e => setSearchQuery(e.target.value)}
-          value={query}
-        />
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          className='absolute top-1/2 left-4 h-6 w-6 -translate-y-1/2 text-black opacity-25 dark:text-white dark:opacity-100'
-          fill='none'
-          viewBox='0 0 24 24'
-          stroke='currentColor'
-          strokeWidth={2}
-        >
-          <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
+      <form onSubmit={handleSubmit}>
+        <div className='relative w-[32rem]'>
+          <input
+            type='search'
+            className='w-full rounded-full py-4 pr-4 pl-14'
+            placeholder='Search posts'
+            onChange={handleChange}
+            value={inputValue}
           />
-        </svg>
-      </div>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='absolute top-1/2 left-4 h-6 w-6 -translate-y-1/2 text-black opacity-25 dark:text-white dark:opacity-100'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
+            />
+          </svg>
+        </div>
+      </form>
     </div>
   );
 }
