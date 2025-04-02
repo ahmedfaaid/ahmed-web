9fd16804a01d43e9ce0535db58a783127dc99049
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-04-02 23:08:01 +0000

    single blog to app router

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,15 +1,12 @@
-import Layout from 'components/layout';
-import MDXImage from 'components/MDXImage';
-import { MDXRemote } from 'next-mdx-remote';
-import { serialize } from 'next-mdx-remote/serialize';
+import MDXImage from '@/components/MDXImage';
+import { getPosts } from '@/utils/getPosts';
+import { MDXRemote } from 'next-mdx-remote/rsc';
 import Image from 'next/image';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeCodeTitles from 'rehype-code-titles';
 import rehypePrism from 'rehype-prism';
 import rehypeSlug from 'rehype-slug';
 import remarkGfm from 'remark-gfm';
-import { BlogPost } from 'types';
-import { getPosts } from 'utils/getPosts';
 
 const mdxComponents = {
   MDXImage,
@@ -41,25 +38,26 @@ const mdxComponents = {
   }
 };
 
-interface SinglePostProps {
-  post: BlogPost;
-  source: any;
-}
+export default async function SinglePost({
+  params
+}: {
+  params: { slug: string };
+}) {
+  const post = (await getPosts()).find(post => post.slug === params.slug);
 
-export default function SinglePost({ post, source }: SinglePostProps) {
   return (
-    <Layout title={post.data.title}>
+    <>
       <h1
         className='my-4 mx-auto text-center font-heading
          text-4xl text-black dark:text-white sm:text-3xl md:w-3/4'
       >
-        {post.data.title}
+        {post?.data.title}
       </h1>
       <div className='mx-auto mt-8 px-4 md:w-3/4'>
         <div className='relative mx-auto h-60 w-full overflow-hidden rounded sm:h-80 md:h-[32rem]'>
           <Image
-            src={post.data.image?.path}
-            alt={post.data.title}
+            src={post?.data.image?.path as string}
+            alt={post?.data.title as string}
             fill
             className='object-cover'
           />
@@ -67,23 +65,44 @@ export default function SinglePost({ post, source }: SinglePostProps) {
         <p className='my-4 text-center font-body text-black dark:text-white'>
           Photo by{' '}
           <a
-            href={post.data.image?.externalLink}
+            href={post?.data.image?.externalLink}
             target='_blank'
             rel='noreferrer noopener'
             className='text-primary'
           >
-            {post.data.image?.creator}
+            {post?.data.image?.creator}
           </a>{' '}
           on Unsplash
         </p>
         <p className='py-6 text-center font-body text-lg font-light italic text-black dark:text-offWhite'>
-          {post.data.description}
+          {post?.data.description}
         </p>
         <div className='mx-auto mb-4 w-full'>
-          <MDXRemote {...source} components={mdxComponents} />
+          <MDXRemote
+            source={post?.content as string}
+            components={mdxComponents}
+            options={{
+              mdxOptions: {
+                remarkPlugins: [remarkGfm],
+                rehypePlugins: [
+                  rehypeSlug,
+                  rehypeCodeTitles,
+                  rehypePrism,
+                  [
+                    rehypeAutolinkHeadings,
+                    {
+                      properties: {
+                        className: ['anchor']
+                      }
+                    }
+                  ]
+                ]
+              }
+            }}
+          />
         </div>
       </div>
-    </Layout>
+    </>
   );
 }
 
@@ -93,34 +112,5 @@ export const getStaticPaths = async () => {
       params: { slug: post.slug }
     })),
     fallback: false
-  };
-};
-
-export const getStaticProps = async ({
-  params
-}: {
-  params: { slug: string };
-}) => {
-  const post = (await getPosts()).find(post => post.slug === params.slug);
-  const source = await serialize(post?.content as string, {
-    mdxOptions: {
-      remarkPlugins: [remarkGfm],
-      rehypePlugins: [
-        rehypeSlug,
-        rehypeCodeTitles,
-        rehypePrism,
-        [
-          rehypeAutolinkHeadings,
-          {
-            properties: {
-              className: ['anchor']
-            }
-          }
-        ]
-      ]
-    }
-  });
-  return {
-    props: { post, source }
   };
 };
