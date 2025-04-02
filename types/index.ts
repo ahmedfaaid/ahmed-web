e88621073d640e589a8555591b7bb89d03a2d9f1
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-04-02 16:22:11 +0000

    published at to string type

--- types/index.ts
+++ types/index.ts
@@ -22,7 +22,7 @@ export interface MdxData {
   description: string;
   image: Image;
   embeddedImagesLocal: EmbeddedImagesLocal;
-  publishedAt: Date;
+  publishedAt: string;
   published: boolean;
 }
 
